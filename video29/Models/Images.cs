923bb2def17e482b6541ccf26b987c102c12e748
EminElekberov <eminelekberov09@gmail.com>
2022-02-02 10:46:30 +0400

    update

---
+++ video29/Models/Images.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace video29.Models
+{
+    public static class Images
+    {
+        public static List<ImagesItem> images = new List<ImagesItem>
+        {
+            new ImagesItem{Id=1,Title="Cloud",Link="1.jpg"},
+            new ImagesItem{Id=2,Title="Cloud",Link="2.jpg"},
+            new ImagesItem{Id=3,Title="Cloud",Link="3.jpg"},
+            new ImagesItem{Id=3,Title="Cloud",Link="3.jpg"},
+            new ImagesItem{Id=3,Title="Cloud",Link="3.jpg"},
+            new ImagesItem{Id=3,Title="Cloud",Link="3.jpg"}
+        };
+        public static List<ImagesItem> GetImagesItems()=> images;
+    }
+    public class ImagesItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+}
