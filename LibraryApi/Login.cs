99088b2cd36dc46225b4a90f0b24fb405326730a
EminElekberov <eminelekberov09@gmail.com>
2022-02-27 15:35:58 +0400

    update

--- LibraryApi/Login.cs
+++ LibraryApi/Login.cs
@@ -25,11 +25,10 @@ namespace LibraryApi
         {
 
         }
-
+        //
         private void BtnLogin_Click(object sender, EventArgs e)
         {
         }
-        //
         private void tabPage2_Click(object sender, EventArgs e)
         {
         }
