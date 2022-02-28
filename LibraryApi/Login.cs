ed92214ab7d6cbb068635120e250de7573322058
EminElekberov <eminelekberov09@gmail.com>
2022-02-28 19:25:00 +0400

    update

--- LibraryApi/Login.cs
+++ LibraryApi/Login.cs
@@ -20,18 +20,16 @@ namespace LibraryApi
             InitializeComponent();
             _db = new LibraryEntities3();
         }
-
+        //
         private void BtnRegister_Click(object sender, EventArgs e)
         {
         }
-        //
         private void BtnLogin_Click(object sender, EventArgs e)
         {
         }
         private void tabPage2_Click(object sender, EventArgs e)
         {
         }
-        //
         private void BtnLogin_Click_1(object sender, EventArgs e)
         {
             string name = txtEmailLogin.Text.Trim();
